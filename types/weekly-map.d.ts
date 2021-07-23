8dfa89570695a772afdff27ddfb66dabb4b588d1
suzuryo <suzuryo@gmail.com>
2021-07-23 12:47:32 +0900

    weekly-mapのsvgの生成をd3からvueに変える

--- types/weekly-map.d.ts
+++ types/weekly-map.d.ts
@@ -4,15 +4,16 @@ type Cities = {
   [key: string]: {
     count: number
     area: string
+    d?: string
   }
 }
 
 type MapDataItem = {
   area: TranslateResult
-  count: number
   label: TranslateResult
   last7days: number
   ruby: TranslateResult
+  d?: string
 }
 
 type MapData = MapDataItem[]
