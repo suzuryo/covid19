6bb89b914f257595ef7a8f33fc9d3cc79765b651
suzuryo <suzuryo@gmail.com>
2021-09-23 11:10:09 +0900

    「お知らせ・参考資料」タブ用のpageを追加

---
+++ pages/reference.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <page-header
+      :icon-path="headerItem.iconPath"
+      :title="headerItem.title"
+      :updated-at="lastUpdateAsString"
+    />
+    <site-top-upper />
+    <cards-tab />
+  </div>
+</template>
+
+<script lang="ts">
+import { mdiChartTimelineVariant } from '@mdi/js'
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import type { MetaInfo } from 'vue-meta'
+
+import PageHeader from '@/components/_shared/PageHeader.vue'
+import CardsTab from '@/components/index/CardsTab.vue'
+import SiteTopUpper from '@/components/index/SiteTopUpper.vue'
+import { lastUpdate } from '@/data/data.json'
+
+type Data = {
+  headerItem: {
+    iconPath: string
+    title: string
+  }
+  lastUpdate: string
+}
+type Methods = {}
+type Computed = {
+  lastUpdateAsString: string
+}
+type Props = {}
+
+const options: ThisTypedComponentOptionsWithRecordProps<
+  Vue,
+  Data,
+  Methods,
+  Computed,
+  Props
+> = {
+  components: {
+    CardsTab,
+    PageHeader,
+    SiteTopUpper,
+  },
+  data() {
+    return {
+      lastUpdate,
+      headerItem: {
+        iconPath: mdiChartTimelineVariant,
+        title: this.$t('Common.お知らせ・参考資料') as string,
+      },
+    }
+  },
+  computed: {
+    lastUpdateAsString() {
+      return this.lastUpdate
+    },
+  },
+  head(): MetaInfo {
+    return {
+      title: this.$t('Common.お知らせ・参考資料') as string,
+    }
+  },
+}
+
+export default options
+</script>
