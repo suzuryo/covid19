3a9ec246bac6aa2ec36a0c84974ad2f860a6d646
suzuryo <suzuryo@gmail.com>
2021-10-13 15:34:10 +0900

    stylelintのルール追加
    https://github.com/MeditationDuck/covid19/pull/2427
    により？ lintがfailするようになっていた

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   rules: {
+    'at-rule-empty-line-before': null,
     'at-rule-no-unknown': null,
+    'rule-empty-line-before': null,
     'scss/at-rule-no-unknown': true,
   },
 }
